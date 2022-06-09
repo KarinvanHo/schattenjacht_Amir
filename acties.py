75589a6c6bb1e176ee42a099071a185e4bac0481
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-09 17:27:18 +0200

    adding inventory functionality

--- acties.py
+++ acties.py
@@ -2,62 +2,126 @@ import time
 
 inventaris = []
 
+
 def uitvoeren(kamer):
-  if kamer == 'inventaris':
-    return inventaris
-  # acties keuken
-  if kamer == 'keuken':
-    while True:
-      keuze = input('''
+	if kamer == 'inventaris':
+		return inventaris
+	# acties keuken
+	if kamer == 'keuken':
+		while True:
+			keuze = input('''
 1. inspecteer het plankje onder de koelkast
 2. inspecteer de deur naar de ketelruimte
 Kies actie 1 of 2 of typ 'terug'\n
 actie: ''')
-      if keuze == str(1):
-        print('''
+			if keuze == str(1):
+				print('''
 Zou het? Niet echt een originele plek voor een cadeau. Je verwijdert het plankje
 en voor je het weet schiet Misha onder de koelkast. POOOEEESSSS. Je kijkt in de
-ruimte maar ziet geen cadeau. Wel een stoffige kat die jer weer onderuit vist.''')
-        time.sleep(2)
-      elif keuze == str(2):
-        print('''
+ruimte maar ziet geen cadeau. Wel een stoffige kat die je er weer onderuit vist.''')
+				time.sleep(2)
+			elif keuze == str(2):
+				print('''
 Doe die deur toch eens dicht! Zo kan ik de klok toch niet lezen??? En het licht uit graag.''')
-        time.sleep(2)
-      elif keuze == 'terug':
-        break
-  # acties loggia
-  if kamer == 'loggia':
-    while True:
-      if 'stofzuiger' not in inventaris:
-        keuze = input('''
-  1. Neem de stofzuiger mee
-  2. actie 2
-  Kies actie 1 of 2 of typ 'terug'\n
+				time.sleep(2)
+			elif keuze == 'terug':
+				break
+	# acties loggia
+	if kamer == 'loggia':
+		while True:
+			if 'stofzuiger' not in inventaris:
+				keuze = input('''
+1. Neem de stofzuiger mee
+2. actie 2
+Kies actie 1 of 2 of typ 'terug'\n
   actie: ''')
-        if keuze == str(1):
-          inventaris.append('stofzuiger')
-          print('''
-  Stofzuiger aan inventaris toegevoegd''')
-          time.sleep(2)
-        elif keuze == str(2):
-          print('''
-  actie 2 gedaan''')
-          time.sleep(2)
-        elif keuze == 'terug':
-          break
-      else:
-        keuze = input('''
+				if keuze == str(1):
+					inventaris.append('stofzuiger')
+					print('''
+Stofzuiger aan inventaris toegevoegd''')
+					time.sleep(2)
+				elif keuze == str(2):
+					print('''
+actie 2 gedaan''')
+					time.sleep(2)
+				elif keuze == 'terug':
+					break
+			else:
+				keuze = input('''
 1. (Stofzuiger heb je al meegenomen)
 2. actie 2
 Kies actie 1 of 2 of typ 'terug'\n
 actie: ''')
-        if keuze == str(2):
-          print('''
+				if keuze == str(2):
+					print('''
 actie 2 gedaan ''')
-          time.sleep(2)
-        elif keuze == 'terug':
-          break
-#   # acties KAMER
+					time.sleep(2)
+				elif keuze == 'terug':
+					break
+	# acties woonkamer
+	elif kamer == 'woonkamer':
+		while True:
+			keuze = input('''
+1. inspecteer bankkussen
+2. actie 2
+Kies actie 1 of 2 of typ 'terug'\n
+actie: ''')
+			if keuze == str(1):
+				if 'stofzuiger' not in inventaris and 'papiertje' not in inventaris:
+					print('''
+Ik zou dit niet zonder stofzuiger doen...''')
+				elif 'papiertje' not in inventaris:
+					stofzuiger = input("Stofzuiger uit je inventaris gebruiken? Typ 'j' of 'n': ").lower()
+					if stofzuiger == 'j':
+						print('''
+STOFZUIGER GEBRUIKT
+Terwijl je de kruimels stofzuigt, ontdek je een papiertje. Je kan deze nog net onderscheppen 
+voordat deze in de stofzuigerslang verdwijnt. Het volgende staat erop geschreven: 
+
+--------------------------
+| a needle in a HAYstack |
+--------------------------
+
+Ok.....''')
+						inventaris.remove('stofzuiger')
+						inventaris.append('papiertje')
+					else:
+						print('Dan toch lekker niet?')
+				else:
+					print(
+						"(Hier heb je het papiertje gevonden met daarop de dubieuze tekst: 'a needle in a HAYstack'.)")
+				time.sleep(2)
+			elif keuze == str(2):
+				print('''
+actie 2 gedaan''')
+				time.sleep(2)
+			elif keuze == 'terug':
+				break
+	############# hier volgt nog slaapkamer! ############
+	# acties studio
+	elif kamer == 'studio':
+		while True:
+			keuze = input('''
+1. luister aandachtig naar de piano.
+2. actie 2
+Kies actie 1 of 2 of typ 'terug'\n
+actie: ''')
+			if keuze == str(1):
+				print('''
+Er valt geen touw aan vast te knopen. Het is zeker geen deuntje. (Of is dit
+experimentele black metal?) Het valt je op dat korte en lange tonen worden
+afgewisseld, met pauzes tussendoor. Morse code! Het klinkt als volgt:
+
+... ___ ...  HIER LOCATIE IN MORSE CODE TYPEN''')
+				time.sleep(2)
+			elif keuze == str(2):
+				print('''
+actie 2 gedaan''')
+				time.sleep(2)
+			elif keuze == 'terug':
+				break
+
+	# acties KAMER
 #   elif kamer == 'KAMER':
 #     while True:
 #       keuze = input('''
--- teksten.py
+++ teksten.py
@@ -2,13 +2,13 @@ spelregels = '''
 *****************
 * De spelregels *
 *****************
-
 - Je kan de ruimtes in je huis bezoeken door de naam van de kamer in te voeren.
-- Voor een overzicht van alle kamers, type 'k'.
-- Je kan verdachte zaken onderzoeken en sommige objecten in je inventaris stoppen.
-- Je inventaris open je door 'i' te typen.
+- Voor een overzicht van alle kamers, typ in het 'ga naar'-veld 'k'.
+- Zodra je een kamer bezoekt kan je twee acties uitvoeren. Dit kan iets inspecteren 
+  zijn, of iets in je inventaris stoppen.
+- Je inventaris open je door in het 'ga naar'-veld 'i' te typen. 
 - Wil je de spelregels opnieuw inzien? Typ 'help'.
-- Wil je het spel verlaten? Print 'exit'
+- Wil je het spel verlaten? Typ 'uitgang'
 '''
 
 start = f'''
@@ -19,8 +19,8 @@ Succes! '''
 
 
 keuken = f'''
-Je begeeft je nu in de keuken. Het plankje onder de koelkast lijkt scheef te zitten. 
-Verder staat de deur naar de ketel op een kier.'''
+Je begeeft je nu in de keuken. Het plankje onder de koelkast lijkt 
+scheef te zitten. Verder staat de deur naar de ketel op een kier.'''
 
 loggia = f'''
 Je bent nu in de loggia. Niks opvallend te zien.'''
@@ -30,7 +30,7 @@ Je begeeft je nu in de slaapkamer.
 '''
 
 studio = f'''
-Je begeeft je nu in de studio.
+Ok, dit is freaky. In de studio speelt het keyboard uit zichzelf.
 '''
 
 toilet = f'''
@@ -42,5 +42,6 @@ Je begeeft je nu in het washok.
 '''
 
 woonkamer = f'''
-Je begeeft je nu in de woonkamer.
+De woonkamer! Genoeg te zien hier, maar valt er iets op? 
+Hmmm, Misha probeert iets tussen de bank vandaan te halen.
 '''
