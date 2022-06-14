e51894892bceccb3430f6a663fa91e0a6fcf7075
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-14 22:22:29 +0200

    completing code (not tested yet!)

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from teksten import *
+import time
 
 
-kamers = {'keuken': keuken_alg, 'loggia': loggia_alg, 'slaapkamer': slaapkamer,
-		  'studio': studio_alg, 'toilet': toilet_alg, 'washok': washok, 'woonkamer': woonkamer_alg}
+kamers = {'keuken': keuken_alg, 'loggia': loggia_alg, 'slaapkamer': slaapkamer_alg,
+		  'studio': studio_alg, 'toilet': toilet_alg, 'washok': washok_alg, 'woonkamer': woonkamer_alg}
 
 
 inventaris = []
-hints = set()
+hints = {5, 6, 7}
 
 
 def print_overzicht(lijst):
@@ -31,11 +32,13 @@ def vervolgactie(vervolg, resultaat_a, resultaat_b):
 		return resultaat_a
 	elif actie == 'n':
 		return resultaat_b
+	else:
+		return 'Ongeldige keuze'
 
 
 def keuzemenu(actie1, actie2, resultaat1, resultaat2, resultaat_a=None, resultaat_b=None, vervolg1=None, vervolg2=None):
 	print(actie1 + '\n' + actie2)
-	keuze = input("Kies actie 1 of actie 2 of typ 'terug': ").lower()
+	keuze = input("\nKies actie 1 of actie 2 of typ 'terug': ").lower()
 	if keuze == 'terug':
 		return
 	elif keuze == str(1) and vervolg1 == 'ja':
@@ -43,7 +46,7 @@ def keuzemenu(actie1, actie2, resultaat1, resultaat2, resultaat_a=None, resultaa
 	elif keuze == str(1):
 		return resultaat1
 	elif keuze == str(2) and vervolg2 == 'ja':
-		return vervolgactie(resultaat1, resultaat_a, resultaat_b)
+		return vervolgactie(resultaat2, resultaat_a, resultaat_b)
 	elif keuze == str(2):
 		return resultaat2
 
@@ -51,7 +54,9 @@ def keuzemenu(actie1, actie2, resultaat1, resultaat2, resultaat_a=None, resultaa
 if __name__ == '__main__':
 	print(start)
 	while True:
-		if len(hints) == 3:
+		if len(hints) == 4:
+			print('\nff wachten...')
+			time.sleep(5)
 			print(einde)
 			break
 		instructie = input('\nGa naar: ').lower()
@@ -87,6 +92,7 @@ if __name__ == '__main__':
 						inventaris.remove('stofzuiger')
 						inventaris.append('papiertje')
 						hints.add(1)
+						print(hints)
 				else:
 					print(keuzemenu(woonkamer_actie1, woonkamer_actie2, woonkamer3_resultaat1, woonkamer_resultaat2))
 			elif instructie == 'toilet':
@@ -101,6 +107,27 @@ if __name__ == '__main__':
 						inventaris.append('dood zilvervisje')
 				else:
 					print(keuzemenu(toilet2_actie1, toilet_actie2, toilet2_resultaat1, toilet_resultaat2))
-
+			elif instructie == 'studio':
+				luisteren = keuzemenu(studio_actie1, studio_actie2, studio_resultaat1, studio_resultaat2)
+				print(luisteren)
+				if luisteren == studio_resultaat2:
+					hints.add(2)
+					print(hints)
+			elif instructie == 'slaapkamer':
+				bekijken = keuzemenu(slaapkamer_actie1, slaapkamer_actie2, slaapkamer_resultaat1, slaapkamer_resultaat2)
+				print(bekijken)
+				if bekijken == slaapkamer_resultaat1:
+					hints.add(3)
+					print(hints)
+			elif instructie == 'washok':
+				if 'bierviltjes' not in inventaris:
+					leeghalen = keuzemenu(washok_actie1, washok_actie2, washok_resultaat1, washok1_resultaat2,
+									resultaat_a=washok_resultaat_a, resultaat_b=washok_resultaat_b, vervolg2='ja')
+					print(leeghalen)
+					if leeghalen == washok_resultaat_a:
+						inventaris.append('bierviltjes')
+						hints.add(4)
+				else:
+					print(keuzemenu(washok_actie1, washok_actie2, washok_resultaat1, washok2_resultaat2))
 		else:
 			print("Controleer je spelling. Bij twijfel raadpleeg de spelregels door 'help' te typen.")
--- teksten.py
+++ teksten.py
@@ -60,48 +60,69 @@ Je hebt alle hints gevonden om je cadeau's te kunnen vinden!
 Hierbij nogmaals de hints:
  
 hint 1:
-a needle in a HAYstack
+Een papiertje met daarop geschreven: 'a needle in a HAYstack'
  
 hint 2:
+Het keyboard wat de volgende noten speelt:
 kort lang lang lang lang pauze
 lang lang kort kort kort pauze
 lang lang kort kort kort pauze
 
 hint 3:
-  
+een schilderij van Julius Caesar die een bordje vasthoudt. Op het bordje staat de letter 'V'. 
+
+hint 4:
+Bierviltjes met daarop de woorden 'mjy', 'fhmyjw', en 'ltwinos'.
+
+
+Aanvullende hints zijn te koop. Succes!
 '''
+
 
 keuken_alg = f'''
 Je begeeft je nu in de keuken. Het plankje onder de koelkast lijkt 
-scheef te zitten. Verder staat de deur naar de ketel op een kier.'''
+scheef te zitten. Verder staat de deur naar de ketel op een kier.
+'''
 keuken_actie1 = '1. inspecteer het plankje onder de koelkast'
 keuken_actie2 = '2. inspecteer de deur naar de ketelruimte'
 keuken_resultaat1 = '''
 Zou het? Niet echt een originele plek voor een cadeau. Je verwijdert het plankje
 en voor je het weet schiet Misha onder de koelkast. POOOEEESSSS. Je kijkt in de
 ruimte maar ziet geen cadeau. Wel een stoffige kat die je er weer onderuit vist.'''
-keuken_resultaat2 = 'Doe die deur toch eens dicht! Zo kan ik de klok toch niet lezen??? En het licht uit graag.'
+keuken_resultaat2 = '\nDoe die deur toch eens dicht! Zo kan ik de klok toch niet lezen??? En het licht uit graag.'
 
 
 loggia_alg = f'''
-Je bent nu in de loggia. Niks opvallend te zien.'''
+Je bent nu in de loggia. Niks opvallend te zien.
+'''
 loggia1_actie1 = '1. Stofzuiger meenemen'
 loggia2_actie1 = '(Je hebt de stofzuiger al meegenomen)'
 loggia_actie2 = '2. Doe het licht uit zodat Karin weer rustig kan ademen op de bank.'
-loggia1_resultaat1 = "De stofzuiger zit nu in je inventaris."
-loggia2_resultaat1 = 'Wat zeg ik nu? Je hebt de stofzuiger al meegenomen.'
-loggia_resultaat2 = "Dankjewel!"
+loggia1_resultaat1 = "\nDe stofzuiger zit nu in je inventaris."
+loggia2_resultaat1 = '\nWat zeg ik nu? Je hebt de stofzuiger al meegenomen.'
+loggia_resultaat2 = "\nDankjewel!"
 
 
-slaapkamer = f'''
-Je begeeft je nu in de slaapkamer.
+slaapkamer_alg = f'''
+In de slaapkamer is het redelijk opgeruimd. Geen hoopjes kleding waaronder iets verscholen kan zitten.
+Misha snuffelt aan een vieze onderbroek die op bed ligt. Huh? Het schilderij is vervangen!
 '''
+slaapkamer_actie1 = '1. Bekijk het schilderij.'
+slaapkamer_actie2 = '2. Geef Misha een aai.'
+slaapkamer_resultaat1 = '''
+Het is een schilderij van Julius Caesar die een bordje vasthoudt. Op het bordje staat de letter 'V'.
+Volgens mij staat deze aanwijzing niet op zichzelf.'''
+slaapkamer_resultaat2 = '''
+MISHA GEAAID.
+En weer door met het spel graag. Misha is er straks ook nog.'''
 
 studio_alg = f'''
-Ok, dit is freaky. In de studio speelt het keyboard uit zichzelf.'''
-studio_actie1 = '1. Luister aandachtig naar de piano'
-studio_actie2 = '2. Actie 2'
-studio_resultaat1 = '''
+Ok, dit is freaky. In de studio speelt het keyboard uit zichzelf.
+'''
+studio_actie1 = '1. Actie 1'
+studio_actie2 = '2. Luister aandachtig naar het keyboard'
+studio_resultaat1 = '\nResultaat1'
+studio_resultaat2 = '''
 Er valt geen touw aan vast te knopen. Het is zeker geen deuntje. (Of is dit
 experimentele black metal?) Het valt je op dat korte en lange tonen worden
 afgewisseld, met pauzes tussendoor. Het klinkt als volgt:
@@ -109,7 +130,7 @@ afgewisseld, met pauzes tussendoor. Het klinkt als volgt:
 kort lang lang lang lang pauze
 lang lang kort kort kort pauze
 lang lang kort kort kort pauze'''
-studio_resultaat2 = 'Resultaat2'
+
 
 toilet_alg = f'''
 Er zit een dik zilvervisje over de toiletbril. Had je nu maar een papiertje om em te weg te halen.
@@ -117,19 +138,30 @@ Er zit een dik zilvervisje over de toiletbril. Had je nu maar een papiertje om e
 toilet1_actie1 = '1. Maak zilvervisje dood.'
 toilet2_actie1 = '(Het zilvervisje heb je verwijderd. Wat je nu ziet is een resterend vlekje van je moordpartij)'
 toilet_actie2 = '2. Scheur velletje toiletpapier af.'
-toilet1_resultaat1 = 'Niet met je blote handen viezerik! Zorg maar eerst dat je een papiertje vindt.'
-toilet2_resultaat1 = 'Wat zeg ik nu? Je hebt het zilvervisje al verwijderd.'
-toilet3_resultaat1 = "Papiertje uit inventaris gebruiken? Typ 'j' of 'n': "
-toilet_resultaat2 = 'Het is lastig zien in een tekstueel avontuur, maar het toiletpapier is OP. Jammer joh!'
+toilet1_resultaat1 = '\nNiet met je blote handen viezerik! Zorg maar eerst dat je een papiertje vindt.'
+toilet2_resultaat1 = '\nWat zeg ik nu? Je hebt het zilvervisje al verwijderd.'
+toilet3_resultaat1 = "\nPapiertje uit inventaris gebruiken? Typ 'j' of 'n': "
+toilet_resultaat2 = '\nHet is lastig zien in een tekstueel avontuur, maar het toiletpapier is OP. Jammer joh!'
 toilet_resultaat_a = '''
 PAPIERTJE GEBRUIKT.
 Zilvervisje gedood. Maar dacht je nu echt dat je hier een hint mee kon vinden? Helaas.
 Je kan wel weer lekker op de toiletbril zitten. Jammer dat het toiletpapier op is.'''
-toilet_resultaat_b = 'Zolang dat zilvervisje er zit kan je geen draak uitlaten hè.'
+toilet_resultaat_b = '\nZolang dat zilvervisje er zit kan je geen draak uitlaten hè.'
 
-washok = f'''
-Je begeeft je nu in het washok.
+washok_alg = f'''
+Het deurtje van de wasmachine is gesloten, maar je kan je niet herinneren dat je 
+een was gedraaid hebt.
 '''
+washok_actie1 = '1. Actie 1.'
+washok_actie2 = '2. Wasmachine openen.'
+washok_resultaat1 = '\nResultaat 1'
+washok1_resultaat2 = "\nInhoud verwijderen? Typ 'j' of 'n': "
+washok2_resultaat2 = "\n(Je hebt hier de bierviltjes gevonden met daarop de woorden 'mjy', 'fhmyjw', en 'ltwinos')"
+washok_resultaat_a = '''
+Geen idee waarom, maar de trommel is gevuld met bierviltjes. Je vindt 3 viltjes met daarop wat geschreven:
+'mjy', 'fhmyjw', en 'ltwinos'.
+'''
+washok_resultaat_b = 'Maar hoe denk je dan verder te kunnen komen met dit spel?'
 
 woonkamer_alg = f'''
 De woonkamer! Genoeg te zien hier, maar valt er iets op? 
@@ -137,10 +169,10 @@ Hmmm, Misha probeert iets tussen de bank vandaan te halen.
 '''
 woonkamer_actie1 = '1. Inspecteer bankkussen'
 woonkamer_actie2 = '2. Actie 2'
-woonkamer1_resultaat1 = 'Ik zou dit niet zonder stofzuiger doen...'
-woonkamer2_resultaat1 = "Stofzuiger gebruiken? Typ 'j' of 'n'"
-woonkamer3_resultaat1 = "(Hier heb je het papiertje gevonden met daarop de dubieuze tekst: 'a needle in a HAYstack'.)"
-woonkamer_resultaat2 = 'Resultaat 2'
+woonkamer1_resultaat1 = '\nIk zou dit niet zonder stofzuiger doen...'
+woonkamer2_resultaat1 = "\nStofzuiger gebruiken? Typ 'j' of 'n'"
+woonkamer3_resultaat1 = "\n(Hier heb je het papiertje gevonden met daarop de dubieuze tekst: 'a needle in a HAYstack'.)"
+woonkamer_resultaat2 = '\nResultaat 2'
 woonkamer_resultaat_a = '''
 STOFZUIGER GEBRUIKT
 Terwijl je de kruimels stofzuigt, ontdek je een papiertje. Je kan deze nog net onderscheppen 
@@ -151,5 +183,5 @@ voordat deze in de stofzuigerslang verdwijnt. Het volgende staat erop geschreven
 --------------------------
 
 Ok.....'''
-woonkamer_resultaat_b = "Dan toch lekker niet?"
+woonkamer_resultaat_b = "\nDan toch lekker niet?"
 
